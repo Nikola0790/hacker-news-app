90917e904a9251a2154be02f779c5de27cb7a04d
Nikola0790 <nikola.drcalic@gmail.com>
2021-09-13 00:31:17 +0200

    small changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./header.css";
 
 export const Header = () => {
   return (
     <header>
-      <div className="head">
-        <h1>Hacker News</h1>
+      <div className="container headColor">
+        <div className="row">
+          <div className="col-12">
+            <nav className="navbar ">
+              <h1>Hacker News</h1>
+            </nav>
+          </div>
+        </div>
       </div>
     </header>
   );
